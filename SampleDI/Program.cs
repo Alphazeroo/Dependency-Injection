39505c34810bb247f4951c72a56ce41614eb8bc8
Alphazeroo <emrekayaa29@gmail.com>
2020-07-10 15:12:11 +0300

    commit

---
+++ SampleDI/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Security.Cryptography;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailOperations mailOperations = new MailOperations();
+            Console.WriteLine("mail yollamak ister misinz ?");
+            Console.Write("1.Evet\n2.Hayır\n");
+            int s1 = Convert.ToInt16(Console.ReadLine());
+            ServiceProvider serviceProvider = new ServiceCollection()
+                                           .AddTransient<IMailService, GmailService>()
+                                           .BuildServiceProvider();
+            if (s1 == 1)
+            {
+                mailOperations.SendMail(serviceProvider.GetService<IMailService>());
+            }
+            else if (s1 == 2)
+            {
+                Console.WriteLine("işlem iptal edildi.");
+            }
+            
+            
+            
+
+
+
+
+
+
+        }
+    }
+}
---
+++ SampleDI/Services.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    public class GmailService : IMailService
+    {
+        public void SendMail()
+        {
+            Console.WriteLine("Gmail ile yollandı.");
+        }
+    }
+
+    
+
+
+}
